805e25eb26109add9404448bbfd3a6f5d01ba401
青衣煮茶 <root@imoi.cn>
2023-01-06 01:39:32 +0000

    update src/Command/JWTCommand.php.
    Signed-off-by: 青衣煮茶 <root@imoi.cn>

--- src/Command/JWTCommand.php
+++ src/Command/JWTCommand.php
@@ -2,18 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * #logic 做事不讲究逻辑，再努力也只是重复犯错
- * ## 何为相思：不删不聊不打扰，可否具体点：曾爱过。何为遗憾：你来我往皆过客，可否具体点：再无你。
- * ## 摔倒一次可以怪路不平鞋不正，在同一个地方摔倒两次，只能怪自己和自己和解，无不是一个不错的选择。
- * @version 1.0.0
- * @author @小小只^v^ <littlezov@qq.com>  littlezov@qq.com
- * @link     https://github.com/littlezo
- * @document https://github.com/littlezo/wiki
- * @license  https://github.com/littlezo/MozillaPublicLicense/blob/main/LICENSE
- *
- */
-
 namespace Xmo\JWTAuth\Command;
 
 use Hyperf\Command\Annotation\Command;
@@ -21,9 +9,9 @@ use Hyperf\Command\Command as HyperfCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-/**
- * @Command
- */
+// https://gitee.com/xmo/jwt-auth  原作者 liyuzhao 现维护者：xmo
+
+#[Command]
 class JWTCommand extends HyperfCommand
 {
     /**
