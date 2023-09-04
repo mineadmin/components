f9116f65c5c754ac3f818e759f33cad0ed4c4f13
X.Mo <root@imoi.cn>
2023-09-04 17:04:46 +0800

    commit

---
+++ src/Controller/AppStoreController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * MineAdmin is committed to providing solutions for quickly building web applications
+ * Please view the LICENSE file that was distributed with this source code,
+ * For the full copyright and license information.
+ * Thank you very much for using MineAdmin.
+ *
+ * @Author X.Mo<root@imoi.cn>
+ * @Link   https://gitee.com/xmo/MineAdmin
+ */
+
+declare(strict_types = 1);
+namespace Mine\Gateway\Controller;
+
+use Mine\Annotation\Auth;
+use Mine\MineController;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\GetMapping;
+use Hyperf\HttpServer\Annotation\PostMapping;
+use Psr\Http\Message\ResponseInterface;
+
+#[Controller(prefix: "mineGateway"), Auth]
+class AppStoreController extends MineController
+{
+    #[GetMapping("hello")]
+    public function hello(): ResponseInterface
+    {
+        return $this->success();
+    }
+}
