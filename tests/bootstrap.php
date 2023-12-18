5a0e155c0cd70148d4414f791d0399c74584f98b
Death-Satan <2771717608@qq.com>
2023-12-18 09:32:18 +0800

    fix phpunit,cs-fix

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+
+declare(strict_types=1);
+
+/**
+ * This file is part of nextphp.
+ *
+ * @link     https://github.com/next-laboratory
+ * @license  https://github.com/next-laboratory/next/blob/master/LICENSE
+ */
+
+ini_set('display_errors', 'on');
+ini_set('display_startup_errors', 'on');
+error_reporting(E_ALL);
+date_default_timezone_set('PRC');
+
+require_once './vendor/autoload.php';
