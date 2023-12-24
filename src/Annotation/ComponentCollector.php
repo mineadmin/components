f695293f7b62ec77a3c5be766dd344bf6669069b
Death-Satan <2771717608@qq.com>
2023-12-24 14:44:52 +0800

    feat 新增Component注解

---
+++ src/Annotation/ComponentCollector.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+
+namespace Mine\Annotation;
+
+use Hyperf\Di\MetadataCollector;
+
+class ComponentCollector extends MetadataCollector
+{
+    protected static array $container = [
+        'component' => [],
+        'postConstruct' => [],
+        'override' => [],
+    ];
+
+    public static function collectComponent(string $class): void
+    {
+        static::$container['component'] = $class;
+    }
+
+    public static function collectPostConstruct(string $class, int $order, $initialStaticCall): void
+    {
+        static::$container['postConstruct'][$class][$order] = $initialStaticCall;
+    }
+
+    public static function collectOverride(string $class, $override): void
+    {
+        static::$container['override'][$class] = $override;
+    }
+
+    public static function getComponent(null|string $component = null): null|array|string
+    {
+        if ($component) {
+            return static::$container['component'][$component] ?? null;
+        }
+        return static::$container['component'];
+    }
+
+    public static function getPostConstruct(null|string $component = null): null|array|string
+    {
+        if ($component) {
+            return static::$container['postConstruct'][$component] ?? null;
+        }
+        return static::$container['postConstruct'];
+    }
+
+    public static function getOverride(null|string $component = null): null|array|string
+    {
+        if ($component) {
+            return static::$container['override'][$component] ?? null;
+        }
+        return static::$container['override'];
+    }
+}
---
+++ src/Annotation/Override.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+
+namespace Mine\Annotation;
+
+use Hyperf\Di\Annotation\AbstractAnnotation;
+use Mine\ServiceException;
+
+#[\Attribute(\Attribute::TARGET_FUNCTION)]
+class Override extends AbstractAnnotation
+{
+    public function collectMethod(string $className, ?string $target): void
+    {
+        $methodReflect = new \ReflectionMethod($className, $target);
+        if (
+            ! $methodReflect->isStatic()
+            || ! $methodReflect->hasReturnType()
+            || ! in_array((string) $methodReflect->getReturnType(), ['self', $className])
+        ) {
+            throw new ServiceException(
+                $className . ' The override annotation is used on static methods and returns an instance of the current class'
+            );
+        }
+
+        ComponentCollector::collectOverride($className, [$className, $target]);
+    }
+}
