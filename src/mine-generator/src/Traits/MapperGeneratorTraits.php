7321eeb054aefbfb4f337a7842afb07ffb02d452
X.Mo <root@imoi.cn>
2024-01-25 23:40:41 +0800

    fix: 添加isset检查

--- src/mine-generator/src/Traits/MapperGeneratorTraits.php
+++ src/mine-generator/src/Traits/MapperGeneratorTraits.php
@@ -49,7 +49,7 @@ trait MapperGeneratorTraits
             return <<<php
 
         // {$comment}
-        if (filled(\$params['{$name}'])) {
+        if (isset(\$params['{$name}']) && filled(\$params['{$name}'])) {
             \$query->where('{$name}', 'like', '%'.\$params['{$name}'].'%');
         }
 
@@ -60,7 +60,7 @@ php;
             return <<<php
 
         // {$comment}
-        if (filled(\$params['{$name}']) && is_array(\$params['{$name}']) && count(\$params['{$name}']) == 2) {
+        if (isset(\$params['{$name}']) && filled(\$params['{$name}']) && is_array(\$params['{$name}']) && count(\$params['{$name}']) == 2) {
             \$query->whereBetween(
                 '{$name}',
                 [ \$params['{$name}'][0], \$params['{$name}'][1] ]
@@ -74,7 +74,7 @@ php;
             return <<<php
 
         // {$comment}
-        if (filled(\$params['{$name}'])) {
+        if (isset(\$params['{$name}']) && filled(\$params['{$name}'])) {
             \$query->whereIn('{$name}', \$params['{$name}']);
         }
 
@@ -85,7 +85,7 @@ php;
             return <<<php
 
         // {$comment}
-        if (filled(\$params['{$name}'])) {
+        if (isset(\$params['{$name}']) && filled(\$params['{$name}'])) {
             \$query->whereNotIn('{$name}', \$params['{$name}']);
         }
 
@@ -95,7 +95,7 @@ php;
         return <<<php
 
         // {$comment}
-        if (filled(\$params['{$name}'])) {
+        if (isset(\$params['{$name}']) && filled(\$params['{$name}'])) {
             \$query->where('{$name}', '{$mark}', \$params['{$name}']);
         }
 
