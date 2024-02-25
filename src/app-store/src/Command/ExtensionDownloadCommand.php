60bc5b08cd0775bbd917570be0328d510e8094ed
Death-Satan <49744633+Death-Satan@users.noreply.github.com>
2024-02-26 00:02:54 +0800

    Code Style

--- src/app-store/src/Command/ExtensionDownloadCommand.php
+++ src/app-store/src/Command/ExtensionDownloadCommand.php
@@ -1,9 +1,19 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+
 namespace Xmo\AppStore\Command;
 
-use Hyperf\Command\Command as Base;
 use Hyperf\Command\Annotation\Command;
+use Hyperf\Command\Command as Base;
 use Hyperf\Context\ApplicationContext;
 use Symfony\Component\Console\Input\InputOption;
 use Xmo\AppStore\Service\AppStoreService;
@@ -15,16 +25,16 @@ class ExtensionDownloadCommand extends Base
 
     protected string $description = 'Download the specified remote plug-in file locally';
 
-    protected function configure()
-    {
-        $this->addOption('name','n',InputOption::VALUE_REQUIRED,'Plug-in Name');
-    }
-
     public function __invoke()
     {
         $name = $this->input->getOption('name');
         $appStoreService = ApplicationContext::getContainer()->get(AppStoreService::class);
         $appStoreService->download($name);
         $this->output->success('Plugin Downloaded Successfully');
+    }
+
+    protected function configure()
+    {
+        $this->addOption('name', 'n', InputOption::VALUE_REQUIRED, 'Plug-in Name');
     }
 }
--- src/app-store/src/Command/ExtensionListCommand.php
+++ src/app-store/src/Command/ExtensionListCommand.php
@@ -1,5 +1,15 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of MineAdmin.
+ *
+ * @link     https://www.mineadmin.com
+ * @document https://doc.mineadmin.com
+ * @contact  root@imoi.cn
+ * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
+ */
+
 namespace Xmo\AppStore\Command;
 
 use Hyperf\Command\Annotation\Command;
@@ -15,20 +25,14 @@ class ExtensionListCommand extends Base
 
     protected string $description = 'View a list of remote app store plugins';
 
-    protected function configure()
-    {
-        $this->addOption('type','t',InputOption::VALUE_OPTIONAL,'Type of plugin to query');
-        $this->addOption('title','title',InputOption::VALUE_OPTIONAL,'Plugin Title');
-    }
-
     public function __invoke()
     {
         $params = [];
         $params['type'] = $this->input->getOption('type');
-        if (empty($params['type'])){
-             $type = 'all';
+        if (empty($params['type'])) {
+            $type = 'all';
         }
-        if ($title = $this->input->getOption('title')){
+        if ($title = $this->input->getOption('title')) {
             $params['title'] = $title;
         }
         $appStoreService = ApplicationContext::getContainer()->get(AppStoreService::class);
@@ -36,6 +40,12 @@ class ExtensionListCommand extends Base
         $headers = [
             'extensionName', 'description', 'author', 'homePage', 'status',
         ];
-        $this->output->table($headers,$result);
+        $this->output->table($headers, $result);
+    }
+
+    protected function configure()
+    {
+        $this->addOption('type', 't', InputOption::VALUE_OPTIONAL, 'Type of plugin to query');
+        $this->addOption('title', 'title', InputOption::VALUE_OPTIONAL, 'Plugin Title');
     }
 }
