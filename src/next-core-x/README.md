de89e84b0a2538589c905f48bd1a1ed968581bd0
Death-Satan <49744633+Death-Satan@users.noreply.github.com>
2024-03-08 10:08:52 +0800

    Added Queue

--- src/next-core-x/README.md
+++ src/next-core-x/README.md
@@ -1,1 +1,5 @@
 # NextCoreX
+
+```shell
+composer require mine/next-core-x
+```
--- src/next-core-x/src/Interfaces/Channel.php
+++ src/next-core-x/src/Interfaces/Channel.php
@@ -14,11 +14,23 @@ namespace Mine\NextCoreX\Interfaces;
 
 interface Channel
 {
+    /**
+     * Push a message to the specified queue.
+     */
     public function push(string $queue, mixed $data): void;
 
+    /**
+     * Pulls a message to the specified queue.
+     */
     public function pull(string $queue): mixed;
 
+    /**
+     * Post a message to the specified queue.
+     */
     public function publish(string $queue, mixed $data): void;
 
+    /**
+     * Listens for messages from the specified queue and blocks the current thread.
+     */
     public function subscribe(string $queue, callable $callback): void;
 }
