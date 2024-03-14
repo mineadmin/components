95cedc8a6bd220bb5d2e07b1ac748d56a655437b
Zds <49744633+zds-s@users.noreply.github.com>
2024-03-14 15:54:13 +0800

    Fixed

--- src/app-store/src/Packer/PackerFactory.php
+++ src/app-store/src/Packer/PackerFactory.php
@@ -12,16 +12,13 @@ declare(strict_types=1);
 
 namespace Xmo\AppStore\Packer;
 
-use Hyperf\Context\ApplicationContext;
-
 final class PackerFactory
 {
     public function get(string $type = 'json'): PackerInterface
     {
-        $container = ApplicationContext::getContainer();
         switch ($type) {
             case 'json':
-                return $container->make(JsonPacker::class);
+                return (new JsonPacker);
             default:
                 throw new \RuntimeException(sprintf('%s Packer type not found', $type));
         }
