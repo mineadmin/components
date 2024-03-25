92c9503605053ff1de886ddd9b08ed0ff444cabf
Zds <49744633+zds-s@users.noreply.github.com>
2024-03-25 16:01:32 +0800

    Merge

--- src/next-core-x/tests/Feature/Channel/RedisChannelTest.php
+++ src/next-core-x/tests/Feature/Channel/RedisChannelTest.php
@@ -15,6 +15,7 @@ use Hyperf\Redis\Redis;
 use Mine\NextCoreX\Channel\RedisChannel;
 use Mine\NextCoreX\Protocols\PhpSerialize;
 use Mine\NextCoreX\ReadConfig;
+use function Hyperf\Support\env;
 
 beforeEach(function () {
     $configInterface = new Config([
@@ -24,6 +25,22 @@ beforeEach(function () {
             ],
         ],
         'serialize' => PhpSerialize::class,
+        'redis' =>  [
+            'default' => [
+                'host' => env('REDIS_HOST', 'localhost'),
+                'auth' => env('REDIS_AUTH', null),
+                'port' => (int) env('REDIS_PORT', 6379),
+                'db' => (int) env('REDIS_DB', 0),
+                'pool' => [
+                    'min_connections' => 1,
+                    'max_connections' => 10,
+                    'connect_timeout' => 10.0,
+                    'wait_timeout' => 3.0,
+                    'heartbeat' => -1,
+                    'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
+                ],
+            ],
+        ]
     ]);
     $this->config = new ReadConfig($configInterface);
     $redis = ApplicationContext::getContainer()->get(Redis::class);
