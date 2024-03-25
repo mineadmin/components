efb7f1c0739a455c75fd7096b3b87050bb6046cc
Zds <49744633+zds-s@users.noreply.github.com>
2024-03-25 12:20:59 +0800

    Update

--- src/HttpServer/src/Exception/Handler/HttpExceptionHandler.php
+++ src/HttpServer/src/Exception/Handler/HttpExceptionHandler.php
@@ -14,26 +14,28 @@ namespace Mine\HttpServer\Exception\Handler;
 
 use Hyperf\ExceptionHandler\Annotation\ExceptionHandler as RegisterExceptionHandler;
 use Hyperf\ExceptionHandler\ExceptionHandler;
+use Hyperf\HttpMessage\Stream\SwooleStream;
 use Mine\HttpServer\Exception\HttpException;
-use Mine\HttpServer\Response;
+use Mine\HttpServer\Result;
 use Psr\Http\Message\ResponseInterface;
 use Swow\Psr7\Message\ResponsePlusInterface;
 
 #[RegisterExceptionHandler]
 class HttpExceptionHandler extends ExceptionHandler
 {
-    public function __construct(
-        private readonly Response $response
-    ) {}
-
     public function handle(\Throwable $throwable, ResponsePlusInterface $response): ResponseInterface
     {
-        return $this
-            ->response
-            ->error(
-                message: $throwable->getMessage(),
-                code: $throwable->getCode(),
-                data: $throwable->result() ?? []
+        /**
+         * @var HttpException $throwable
+         */
+        $result = Result::error(
+            message: $throwable->getMessage(),
+            data: $throwable->result(),
+            code: $throwable->getCode()
+        );
+        return $response
+            ->setBody(
+                new SwooleStream((string) $result)
             );
     }
 
