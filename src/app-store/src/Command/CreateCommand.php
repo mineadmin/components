bd6ddf8a0743494bde2b20b0ff591439df6b4ba7
NKEGod <39877796+NEKGod@users.noreply.github.com>
2024-04-17 17:50:22 +0800

    Optimisation plugin autogeneration (#68)

--- src/app-store/src/Command/CreateCommand.php
+++ src/app-store/src/Command/CreateCommand.php
@@ -30,6 +30,10 @@ class CreateCommand extends AbstractCommand
         $name = $this->input->getOption('name');
         $type = $this->input->getOption('type') ?? 'mix';
         $type = PluginTypeEnum::fromValue($type);
+        if (empty($name)) {
+            $this->output->error('Plugin name is empty');
+            return;
+        }
         if ($type === null) {
             $this->output->error('Plugin type is empty');
             return;
@@ -52,11 +56,6 @@ class CreateCommand extends AbstractCommand
         $this->createMineJson($pluginPath, $name, $type);
     }
 
-    public function commandName(): string
-    {
-        return 'create';
-    }
-
     public function createMineJson(string $path, string $name, PluginTypeEnum $pluginType): void
     {
         $output = new \stdClass();
@@ -71,11 +70,12 @@ class CreateCommand extends AbstractCommand
             ],
         ];
         if ($pluginType === PluginTypeEnum::Backend || $pluginType === PluginTypeEnum::Mix) {
-            $namespace = 'Mine\\' . Str::snake($author) . '\\' . Str::snake($name);
+            $namespace = 'Plugin\\' . Str::studly($name);
 
             $this->createInstallScript($namespace, $path);
             $this->createUninstallScript($namespace, $path);
             $this->createConfigProvider($namespace, $path);
+            $this->createViewScript($namespace, $path);
             $output->composer = [
                 'require' => [],
                 'psr-4' => [
@@ -99,26 +99,10 @@ class CreateCommand extends AbstractCommand
         $this->output->success(sprintf('%s 创建成功', $path . '/mine.json'));
     }
 
-    public function createConfigProvider(string $namespace, string $path): void
-    {
-        $installScript = $this->buildStub('ConfigProvider', compact('namespace'));
-        $installScriptPath = $path . '/src/ConfigProvider.php';
-        file_put_contents($installScriptPath, $installScript);
-        $this->output->success(sprintf('%s Created Successfully', $installScriptPath));
-    }
-
     public function createInstallScript(string $namespace, string $path): void
     {
         $installScript = $this->buildStub('InstallScript', compact('namespace'));
         $installScriptPath = $path . '/src/InstallScript.php';
-        file_put_contents($installScriptPath, $installScript);
-        $this->output->success(sprintf('%s Created Successfully', $installScriptPath));
-    }
-
-    public function createUninstallScript(string $namespace, string $path): void
-    {
-        $installScript = $this->buildStub('UninstallScript', compact('namespace'));
-        $installScriptPath = $path . '/src/UninstallScript.php';
         file_put_contents($installScriptPath, $installScript);
         $this->output->success(sprintf('%s Created Successfully', $installScriptPath));
     }
@@ -141,6 +125,27 @@ class CreateCommand extends AbstractCommand
         return realpath(__DIR__) . '/Stub';
     }
 
+    public function createUninstallScript(string $namespace, string $path): void
+    {
+        $installScript = $this->buildStub('UninstallScript', compact('namespace'));
+        $installScriptPath = $path . '/src/UninstallScript.php';
+        file_put_contents($installScriptPath, $installScript);
+        $this->output->success(sprintf('%s Created Successfully', $installScriptPath));
+    }
+
+    public function createConfigProvider(string $namespace, string $path): void
+    {
+        $installScript = $this->buildStub('ConfigProvider', compact('namespace'));
+        $installScriptPath = $path . '/src/ConfigProvider.php';
+        file_put_contents($installScriptPath, $installScript);
+        $this->output->success(sprintf('%s Created Successfully', $installScriptPath));
+    }
+
+    public function commandName(): string
+    {
+        return 'create';
+    }
+
     protected function configure()
     {
         $this->addArgument('path', InputArgument::REQUIRED, 'Plugin Path');
@@ -148,5 +153,10 @@ class CreateCommand extends AbstractCommand
         $this->addOption('type', 'type', InputOption::VALUE_OPTIONAL, 'Plugin type, default mix optional mix,frond,backend');
         $this->addOption('description', 'desc', InputOption::VALUE_OPTIONAL, 'Plug-in Introduction');
         $this->addOption('author', 'author', InputOption::VALUE_OPTIONAL, 'Plugin Author Information');
+    }
+
+    private function createViewScript(string $namespace, string $path): void
+    {
+        ! is_dir($path . '/web') && mkdir($path . '/web', 0775);
     }
 }
