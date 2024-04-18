a6ef8ae0d840bdbf73b04b61aeeeff9be99b3465
Zds <49744633+zds-s@users.noreply.github.com>
2024-04-19 00:24:16 +0800

    Update

--- .travis/run.code-coverage.sh
+++ .travis/run.code-coverage.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-php bin/hyperf.php migrate --path=app/Setting/Database/Migrations
-
-php bin/hyperf.php migrate --path=app/System/Database/Migrations
-
-php bin/hyperf.php db:seed --path=app/Setting/Database/Seeders
-
-php bin/hyperf.php db:seed --path=app/System/Database/Seeders
-
-php bin/hyperf.php mine:update
 
 php ./vendor/bin/pest --coverage --coverage-clover tests/coverage.xml
