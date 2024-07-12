f722c3c359d666b5eda49a2d29256e5e7b5abbad
zds <49744633+zds-s@users.noreply.github.com>
2024-07-12 23:59:42 +0800

    Remove Automatic Installation of App Store

--- src/mine-core/src/Command/InstallProjectCommand.php
+++ src/mine-core/src/Command/InstallProjectCommand.php
@@ -95,9 +95,6 @@ class InstallProjectCommand extends MineCommand
             $this->call('vendor:publish', ['package' => 'xmo/mine']);
         }
 
-        // 安装插件
-        Plugin::install('mine-admin/app-store');
-
         $downloadFrontCode = $this->confirm('Do you downloading the front-end code to "./web" directory?', true);
 
         // 下载前端代码
